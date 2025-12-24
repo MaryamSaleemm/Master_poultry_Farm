5146cfeafaacf57300423860843415cf585da4b8
MaryamSaleemm <maryamsaleem176@gmail.com>
2025-12-24 16:09:03 +0500

    initial commit

--- master_poultry_app/android/app/src/main/kotlin/com/example/master_poultry_app/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.example.master_poultry_app
-
-// BAD (Old V1): import io.flutter.app.FlutterActivity
-// GOOD (New V2):
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
